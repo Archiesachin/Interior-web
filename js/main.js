ad4440fa9ef30b2821daf01018ac5ddea59428a5
ArchieSachin <archieshah8767@gmail.com>
2024-09-09 11:58:42 +0530

    animations added

--- js/main.js
+++ js/main.js
@@ -4,3 +4,77 @@ const navMenu = document.querySelector('#nav_menu')
 navTrigger.addEventListener('click', () =>{
     navMenu.classList.toggle('nav-is-open')
 })
+
+
+
+
+
+//swiper
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination:{
+        el: '.swiper-pagination',
+        clickable: true, 
+    },
+    slidesPerView: 3, 
+    spaceBetween: 20,
+    breakpoints:{
+        320:{
+            slidesPerView: 1
+        }, 
+        960:{
+            slidesPerView: 2
+        },
+        1200:{
+            slidesPerView: 3
+        }
+    }
+})
+
+
+//scroll reveal 
+const sr = ScrollReveal({
+    origin: 'bottom',
+    diatance: '60px',
+    duration: 3000,
+    delay: 600, 
+    // reset: true
+})
+
+//hero
+sr.reveal('.hero__text', {origin: 'top'});
+
+//steps
+sr.reveal('.steps__step', {distance: '100px', interval: 80
+})
+
+//about
+sr.reveal('.about__text', {origin: 'left'})
+sr.reveal('.about__image', {origin: 'right', delay: 80})
+
+//testimonial
+sr.reveal('.testimonial__bg', {delay: 800})
+sr.reveal('.testimonial__title')
+sr.reveal('.testimonial__slider', {delay: 1000})
+
+//brands
+sr.reveal('.brands__img', {delay: 600, distance:'100px', interval: 100})
+
+//works
+sr.reveal('.work__title')
+sr.reveal('.work__subtitle', {delay:800})
+sr.reveal('.work__grid', {delay:100})
+
+//contact
+sr.reveal('.contact__container')
+sr.reveal('.contact__text', {delay:800})
+
+//footer
+sr.reveal('.footer__item', {
+    distance:'100px',
+    interval: 100, 
+    delay: 1000
+})
+
+sr.reveal('.footer__copyright')
+
